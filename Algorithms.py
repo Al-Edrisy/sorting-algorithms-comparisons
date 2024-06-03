b196ac5a7f41cbed91b5392f1431b2c5d7684e9d
Al-Edrisy <salehfree33@gmail.com>
2024-06-03 20:14:54 +0300

    Version 1.0.1

--- Algorithms.py
+++ Algorithms.py
@@ -61,3 +61,39 @@ class SortingAlgorithms:
                 j -= 1
             arr[j + 1] = key
         return arr
+
+    @staticmethod
+    def bubble_sort(arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        return arr
+
+    @staticmethod
+    def heap_sort(arr):
+        def heapify(arr, n, i):
+            largest = i
+            l = 2 * i + 1
+            r = 2 * i + 2
+
+            if l < n and arr[l] > arr[largest]:
+                largest = l
+
+            if r < n and arr[r] > arr[largest]:
+                largest = r
+
+            if largest != i:
+                arr[i], arr[largest] = arr[largest], arr[i]
+                heapify(arr, n, largest)
+
+        n = len(arr)
+        for i in range(n // 2 - 1, -1, -1):
+            heapify(arr, n, i)
+
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            heapify(arr, i, 0)
+
+        return arr
